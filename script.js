f002c48d8c887662cb17b43dee759acb9e308ee8
Johns189 <fkodeloftet@gmail.com>
2024-10-09 13:03:52 +0200

    todo-list-became-permanent

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 let userInput = document.querySelector("#user-input")
 let todolistElement = document.querySelector("#todo-list")
 
-let todos = []
-//Dette er det som skal skje når brukeren trykker på legg till knappen
+/* //originalen
+let toStore = [1, 2, 3, 4]
+
+//Oversett og lagre i LocalStorage
+let jsonToStore = JSON.stringify(toStore)
+localStorage.setItem("todos", jsonToStore);
+
+//Hent ut og oversett tilbake til JavaScript
+let storedItem = localStorage.getItem("todos");
+let converted = JSON.parse(storedItem)
+
+//Logg ut originalen og det vi har hentet ut fra LocalStorage
+console.log(toStore)
+console.log(converted) */
 
 
+let storedTodos = localStorage.getItem("todos");
+let convertedTodos = JSON.parse(storedTodos)
+let todos = convertedTodos;
+
+renderTodos();
+
+//Dette er det som skal skje når brukeren trykker på 
+//Legg till knappen
 
 function handleSubmit(event) {
     event.preventDefault(); //Forhindrer nettsiden og lastes inn på nytt
@@ -15,20 +35,12 @@ function handleSubmit(event) {
     console.log("Append new todo to todo list..." );
     todos.push(newTodo);
 
-    console.log("Clearing out the old todos from the document...")
-    todolistElement.innerHTML = "";
-    
-    console.log("Appending all todos to the Document...");
-      
-    todos.forEach((todo, index) => {
-        console.log(todo)
-        console.log(index)
-        //Først lege det HTML for det gjøremålet
-        let newTodoCard = createTodoCard(todo);
+    console.log("Uppdating the stored list...")
+    let jsonTodos = JSON.stringify(todos)
+    localStorage.setItem("todos",jsonTodos)
 
-        //Legg det nye html elementet til i Dokumentet
-        todolistElement.append(newTodoCard);
-    });
+    renderTodos();
+    
     }
 
 
@@ -62,4 +74,21 @@ function createTodoCard(todoObject) {
     titleElement.textContent = todoObject.title;
 
 return todoCard;
+}
+
+//Den har jobben og oppdatere HTML fra gjøremålene
+function renderTodos() {
+    console.log("Clearing out the old todos from the document...")
+    todolistElement.innerHTML = "";
+    
+    console.log("Appending all todos to the Document...");
+    todos.forEach((todo, index) => {
+        console.log(todo)
+        console.log(index)
+        //Først lege det HTML for det gjøremålet
+        let newTodoCard = createTodoCard(todo);
+
+        //Legg det nye html elementet til i Dokumentet
+        todolistElement.append(newTodoCard);
+    });   
 }
